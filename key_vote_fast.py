05fce3b06b68da4192eef872154745847d737321
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-07-16 00:30:53 +0200

    key_vote_fast utilise la meme representation interne que key_vote, et sauve ses resultats tries
    darcs-hash:20070715223053-2fc9d-8dd9132bab68037d190bdf95eacc8ea15addc2a3.gz

--- key_vote_fast.py
+++ key_vote_fast.py
@@ -21,127 +21,49 @@ import key_vote_lib
 
 if __name__ == '__main__':
   results = ([],[]) # tableau contenant les résultats (un pour chaque main)
-  sorted = ([],[]) # tableau contenant les résultats triés (un pour chaque main)
-  scores = {} # tableau associatif des scores par touche
   matchs = {} # matchs réellement effectués
   
-  if len( sys.argv ) == 1 :
+  if len( sys.argv ) < 2 :
+    sortedResultFile = 'result-sorted'
+    key_vote_lib.printStdOut( u'Pas de fichier spécifié, utilisation du fichier ' + sortedResultFile )
+  else:
+    sortedResultFile = sys.argv[1]
+
+  if len( sys.argv ) < 3 :
     resultFile = 'result'
     key_vote_lib.printStdOut( u'Pas de fichier spécifié, utilisation du fichier ' + resultFile )
   else:
-    resultFile = sys.argv[1]
+    resultFile = sys.argv[2]
 
   # choix du clavier
   kbd = key_vote_lib.chooseKbd()
+  
+  if os.path.exists( sortedResultFile ):
+    results = pickle.load( file( sortedResultFile ) )
+  else:
+    results = ([],[])
   
   if os.path.exists( resultFile ):
     matchs = pickle.load( file( resultFile ) )
   else:
     matchs = {}
   
-  # affichage des matchs à charger
-  #for pair in matchs.keys():
-    #k1 = key_vote_lib.posToString(pair[0], kbd)
-    #k2 = key_vote_lib.posToString(pair[1], kbd)
-    #print '(' + k1 + ',' + k2 + ') : ' + str(matchs[pair])
-  
-  # prise en compte des matchs déjà joué
-  for pair in matchs.keys():
-    k1 = key_vote_lib.posToString(pair[0], kbd)
-    k2 = key_vote_lib.posToString(pair[1], kbd)
-    if (k1 in kbd[0] and k2 in kbd[0]) or (k1 in kbd[1] and k2 in kbd[1]):
-      if k1 in kbd[0]:
-        handID = 0
-      else:
-        handID = 1
-      
-      for c in (k1,k2):
-        # si on a déjà classé la touche, on passe à celle d'après
-        already = False
-        for keys in results[handID]:
-          if c in keys:
-            already = True
-            break
-        if already == True:
-          break
-        
-        if len(results[handID]) == 0:
-          results[handID].append((c,))
-        else: # dichotomie, voir dans les tests pour l'algorithme
-          min = 0
-          max = len(results[handID])-1
-          
-          matchfound = True
-          while min <= max and matchfound == True:
-            middle = (max+min)/2
-            
-            test = results[handID][middle][0] # on va tester par rapport à ce caractère
-            
-            if c == test:
-              break
-            
-            matchfound = False
-            for pair in matchs.keys():
-              if matchfound == True:
-                print 'BIG WARNING'
-              pair0 = key_vote_lib.posToString(pair[0], kbd)
-              pair1 = key_vote_lib.posToString(pair[1], kbd)
-              if pair0 == c and pair1 == test:
-                if matchs[pair] == 0: # key == test
-                  min = max+1
-                  results[handID][middle] += (c,)
-                  matchfound = True
-                  break
-                elif matchs[pair] == 1: # key > test
-                  min = middle+1
-                  if max < min:
-                    results[handID].insert(middle+1, (c,))
-                  matchfound = True
-                  break
-                elif matchs[pair] == 2: # key < test
-                  max = middle-1
-                  if max < min:
-                    results[handID].insert(middle, (c,))
-                  matchfound = True
-                  break
-              elif pair0 == test and pair1 == c:
-                if matchs[pair] == 0: # key == test
-                  min = max+1
-                  results[handID][middle] += (c,)
-                  matchfound = True
-                  break
-                elif matchs[pair] == 2: # key > test
-                  min = middle+1
-                  if max < min:
-                    results[handID].insert(middle+1, (c,))
-                  matchfound = True
-                  break
-                elif matchs[pair] == 1: # key < test
-                  max = middle-1
-                  if max < min:
-                    results[handID].insert(middle, (c,))
-                  matchfound = True
-                  break
-            
-            #if matchfound == False:
-              #key_vote_lib.printStdOut( '"' + c + u'" : match avec "' + test + u'" non trouvé. pas d\'insertion' )
-            
-    else:
-      key_vote_lib.printStdOut( k1 + ' et ' + k2 + ' sont sur des mains différentes!' )
-  
-  
   # ces touches sont sous les doigts et gagnent toujours
-  win = kbd[0][11:15] + kbd[1][15:19]
+  win = [ key_vote_lib.stringToPos( c, kbd ) for c in kbd[0][11:15] + kbd[1][15:19] ]
   
   run = True
+  
+  nbOfChars = 1
   
   # tests (pour chaque main)
   for handID in (0,1):
     if not run:
       break
     # création d'un array contenant tous les caractères mélangés
-    untested = array('u')
-    untested.fromunicode(kbd[handID])
+    untested = []
+    for c in kbd[handID]:
+    	# à faire : gérer plusieurs charactères
+      untested.append( key_vote_lib.stringToPos( c, kbd ) )
     
     # on enlève ce qu'on a déjà testé (sauvegardé)
     for keys in results[handID]:
@@ -158,6 +80,7 @@ if __name__ == '__main__':
     # tant qu'on a pas testé tous les caractères
     while len(untested) > 0 and run:
       c = untested[0] # on récupère le premier caractère correspondant
+      C = key_vote_lib.posToString( c, kbd )
       
       if len(results[handID]) == 0: # si le tableau de résultats est vide
         results[handID].append((c,)) # on ajoute notre caractère
@@ -170,6 +93,7 @@ if __name__ == '__main__':
           middle = (max+min)/2 # on choisit le milieu (dichotomie)
           
           test = results[handID][middle][0] # on va tester c par rapport à ce caractère
+          TEST = key_vote_lib.posToString( test, kbd )
           
           pair = ( c, test )
           
@@ -183,32 +107,31 @@ if __name__ == '__main__':
             res = None
             ires = 2
           else:
-            key_vote_lib.printKbdLayout(kbd, (c,test) )
-            key_vote_lib.printStdOut( u"1: %s  2: %s   0: égalité   Q: quitter  (%i touches restantes sur cette main)" % ( c, test, len(untested) ) )
-            res = key_vote_lib.readResult( 'vote: ', ['0', '1', '2', 'Q', c, test] )
+            key_vote_lib.printKbdLayout(kbd, (C,TEST) )
+            key_vote_lib.printStdOut( u"1: %s  2: %s   0: égalité   Q: quitter  (%i touches restantes sur cette main)" % ( C, TEST, len(untested) ) )
+            res = key_vote_lib.readResult( 'vote: ', ['0', '1', '2', 'Q', C, TEST] )
             ires = key_vote_lib.zeroOneTwo( res )
           
           if res == 'Q':
             run = False
           
-          key = (key_vote_lib.stringToPos(c, kbd),key_vote_lib.stringToPos(test, kbd))
-          
-          if ires == 1 or res == c: # le caractère est meilleur que le milieu
+          if ires == 1 or res == C: # le caractère est meilleur que le milieu
             min = middle+1 # le minimum est maintenant après le milieu
             if max < min:
               results[handID].insert(middle+1, (c,)) # insertion avant le milieu
-            matchs[key] = 1
-          elif ires == 2 or res == test: # le caractère est moins bon que le milieu
+            matchs[pair] = 1
+          elif ires == 2 or res == TEST: # le caractère est moins bon que le milieu
             max = middle-1 # le maximum est avant le milieu
             if max < min:
               results[handID].insert(middle, (c,)) # insertion après le milieu
-            matchs[key] = 2
+            matchs[pair] = 2
           elif ires == 0: # les caractères sont aussi bons
             results[handID][middle] += (c,) # on ajoute au milieu
             max = min-1 # fin du test de ce caractère
-            matchs[key] = 0
+            matchs[pair] = 0
         
         pickle.dump( matchs, file( resultFile, "w" ) )
+        pickle.dump( results, file( sortedResultFile, "w" ) )
         
       untested.pop(0) # on supprime le caractère de l'array
     
@@ -220,9 +143,9 @@ if __name__ == '__main__':
     for (i,result) in enumerate(results[handID]): # pour chaque ensemble de touches
       currentLess = []
       for char in result: # pour chaque touche
-      	candidate1 = key_vote_lib.stringToPos( char, kbd )
+      	candidate1 = char
       	for l in less:
-      		candidate2 = key_vote_lib.stringToPos( l, kbd )
+      		candidate2 = l
       		if candidate1 < candidate2:
       		  base_results[ ( candidate1, candidate2 ) ] = 1
       		else:
